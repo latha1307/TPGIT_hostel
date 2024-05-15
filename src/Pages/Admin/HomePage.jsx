4260a884f72d9f312425b98048d7e27db9eabbae
latha1307 <ldkannammal@gmail.com>
2024-05-15 13:05:31 +0530

    Adjusted admin page  height

--- src/Pages/Admin/HomePage.jsx
+++ src/Pages/Admin/HomePage.jsx
@@ -4,7 +4,7 @@ import { ANavbar } from "../../components/Admin/ANavbar";
 function HomePage () {
     return (
         <>
-            <div className="bg-gray-600 flex flex-col h-auto justify-center p-10 pt-2 pb-2">
+            <div className="bg-gray-600 flex flex-col h-[100vh] justify-center p-10 pt-2 pb-2">
                 <div className="bg-white ">
                     <ANavbar/>
                 </div>
