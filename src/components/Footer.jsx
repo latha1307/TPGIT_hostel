438667141a20aa2dafe6404fba397a84cd0517b1
latha1307 <ldkannammal@gmail.com>
2024-05-17 11:53:00 +0530

    corrected

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -69,6 +69,9 @@ export const Footer = () => {
                         <li>Lathadevi B</li>
                         <li>Bhavani R</li>
                         <li>Jana K</li>
+                        <li>Agarsha S</li>
+                        <li>Iniya G</li>
+                        <li></li>
                     </ul>
                 </div>
             </div>
