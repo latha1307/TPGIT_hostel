ebd0edddf59504709a05215bd12496492b54d8a6
latha1307 <ldkannammal@gmail.com>
2024-05-17 15:09:51 +0530

    Added Page Transition

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,41 @@
-
-import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+// src/App.js
+import './App.css';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import { AnimatePresence, motion } from 'framer-motion';
 import Home from './Pages/Home';
 import About from './Pages/About';
 import NewRegister from './Pages/NewRegistration';
-import ExistingRegistration from './Pages/ExistingRegistration'
+import ExistingRegistration from './Pages/ExistingRegistration';
 import AdminLogin from './Pages/Login/AdminLogin';
 import StudentLogin from './Pages/Login/StudentLogin';
 import SHomePage from './Pages/Student/HomePage';
 import Vacate from './Pages/Student/Vacate';
 import AHomePage from './Pages/Admin/HomePage';
+import Transition from './components/Transition';
 
 function App() {
+  const location = useLocation();
+
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-        <Route path='/' element={<Home/>}/>
-          <Route path='/home' element={<Home/>}/>
-          <Route path='/about' element={<About/>}/>
-          <Route path='/new-student-register' element={<NewRegister/>}/>
-          <Route path='/existing-student-register' element={<ExistingRegistration/>}/>
-          <Route path='/admin-login' element={<AdminLogin/>}/>
-          <Route path='/student-login' element={<StudentLogin/>}/>
-          <Route path='/student-profile' element={<SHomePage />}/>
-          <Route path='/student-vacate' element={<Vacate />}/>
-          <Route path='/admin-home' element={<AHomePage />}/>
-          
+
+    <AnimatePresence mode='wait'>
+      <motion.div key={location.pathname} className="relative">
+        <Transition />
+        <Routes location={location} key={location.pathname}>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/new-student-register" element={<NewRegister />} />
+          <Route path="/existing-student-register" element={<ExistingRegistration />} />
+          <Route path="/admin-login" element={<AdminLogin />} />
+          <Route path="/student-login" element={<StudentLogin />} />
+          <Route path="/student-profile" element={<SHomePage />} />
+          <Route path="/student-vacate" element={<Vacate />} />
+          <Route path="/admin-home" element={<AHomePage />} />
         </Routes>
-        
-      </BrowserRouter>
-    </>
-  )
+      </motion.div>
+    </AnimatePresence>
+  );
 }
 
-export default App; 
+export default App;
